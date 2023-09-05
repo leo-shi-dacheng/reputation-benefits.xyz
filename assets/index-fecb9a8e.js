1955e5a9f5784d021f6b01ef3390a7bd13bbc834
leo-shi-dacheng <554601845@qq.com>
2023-09-05 13:55:02 +0800

    Add files via upload

---
+++ assets/index-fecb9a8e.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-61eafc04.js";const r=()=>e("div",{className:"home h-screen font-sans tracking-wider",children:e("div",{className:"flex items-center",children:e("div",{className:"g-parallax-container",children:"login"})})});export{r as default};
+//# sourceMappingURL=index-fecb9a8e.js.map
