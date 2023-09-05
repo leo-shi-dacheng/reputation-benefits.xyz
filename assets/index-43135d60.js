246732b2012af22982702b6135fc5514dae36e73
leo-shi-dacheng <554601845@qq.com>
2023-09-05 15:40:45 +0800

    1

--- assets/index-43135d60.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./.pnpm-3ed82fb7.js";const r=()=>e("div",{className:"home h-screen font-sans tracking-wider",children:e("div",{className:"flex items-center",children:e("div",{className:"g-parallax-container",children:"login"})})});export{r as default};
