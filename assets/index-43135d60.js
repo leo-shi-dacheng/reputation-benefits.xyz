009ad5dca70c6c10540e1a33404bbf19c331fa56
leo-shi-dacheng <554601845@qq.com>
2023-09-05 11:58:02 +0800

    Add files via upload

---
+++ assets/index-43135d60.js
@@ -0,0 +1,2 @@
+import{j as e}from"./.pnpm-3ed82fb7.js";const r=()=>e("div",{className:"home h-screen font-sans tracking-wider",children:e("div",{className:"flex items-center",children:e("div",{className:"g-parallax-container",children:"login"})})});export{r as default};
+//# sourceMappingURL=index-43135d60.js.map
